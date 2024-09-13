9537352ee006fcc8aba8c9cf58fbd1e714e0c075
GastonFlorenciano <gaston__florenciano@hotmail.com>
2024-09-13 15:00:17 -0300

    CREACION DE LA FUNCION DE CREAR TAREA, SOLO EN EL BACKEND

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -1,8 +1,9 @@
 import { database } from "../db/database.js";
 
+
 export const getAllTodosCtrl = (req, res) => {
   const todos = database.todos;
-  
+
   const tasksCont = [];
 
   const user = req.user;
@@ -12,10 +13,75 @@ export const getAllTodosCtrl = (req, res) => {
     if (task.owner === user.id) {
       tasksCont.push(task);
     }
+
   });
-  
-  console.log(tasksCont);
-  
+
+  tasksCont.map((task, index) => {
+    task.id = index + 1;
+  });
+
   res.json(tasksCont);
 
 };
+
+export const createTodoCtrl = (req, res) => {
+
+  try {
+
+
+    const todos = database.todos;
+
+    const lastId = todos[todos.length - 1].id;
+
+    const { title, completed } = req.body;
+
+    const newTask = {
+      id: lastId + 1,
+      title,
+      completed,
+      owner: req.user.id,
+    };
+
+    todos.push(newTask);
+
+    res.status(201).json({message: 'Tarea creada exitosamente'});
+
+  } catch (error) {
+
+    console.error(error);
+    return res.status(500).json({ message: "Error Inesperado" });
+
+  }
+
+
+}
+
+export const updateTodoCtrl = (req, res) => {
+
+  try {
+
+    const { id } = req.params;
+
+    const { title, completed } = req.body;
+
+    const todos = database.todos;
+
+    const task = todos.find(task => task.id === Number(id));
+
+    if (!task) {
+      return res.status(404).json({ message: "Tarea no encontrada" });
+    }
+
+    task.title = title;
+    task.completed = completed;
+
+    res.json({ message: "Tarea actualizada" });
+
+  } catch (error) {
+
+    console.error(error);
+    return res.status(500).json({ message: "Error Inesperado" });
+
+  }
+
+}
--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllTodosCtrl } from "../controllers/todos.controllers.js";
+import { createTodoCtrl, getAllTodosCtrl, updateTodoCtrl } from "../controllers/todos.controllers.js";
 import validarJwt from "../middlewares/validar-jwt.js";
 
 const todosRouter = Router();
 
 todosRouter.get("/", validarJwt, getAllTodosCtrl);
+todosRouter.post("/create", validarJwt, createTodoCtrl);
+todosRouter.put("/edit", validarJwt, updateTodoCtrl);
 
 export { todosRouter };
